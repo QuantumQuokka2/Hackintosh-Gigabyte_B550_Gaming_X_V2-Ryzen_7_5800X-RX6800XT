73b36f586361beeef46cf3f43d3c5a45dfbd1088
QuantumQuokka <123481656+QuantumQuokka2@users.noreply.github.com>
2024-10-02 20:22:01 +0300

    Update Hidden.bat

--- Hidden.bat
+++ Hidden.bat
@@ -1,3 +1,3 @@
 @echo off
-powershell -WindowStyle Hidden -ExecutionPolicy Bypass -File "C:\path\to\DisableUACBypass.ps1"
+powershell -WindowStyle Hidden -ExecutionPolicy Bypass -File "C:\Users\dukew\Documents\F.ps1"
 exit
